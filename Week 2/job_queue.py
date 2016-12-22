ae776680a6ee8ce18cda2e5b453287dba157a623
Rajendra Kadam <rajendrakadam249@gmail.com>
2016-12-22 19:28:09 +0530

    Parallel processing

--- Week 2/job_queue.py
+++ Week 2/job_queue.py
@@ -1,28 +1,45 @@
 # python3
+import heapq
+class Worker():
+  def __init__(self, thread_id, release_time = 0):
+    self.thread_id = thread_id
+    self.release_time = release_time
+
+  def __lt__(self, other):
+    if self.release_time == other.release_time:
+      return self.thread_id < other.thread_id
+    return self.release_time < other.release_time
+
+  def __gt__(self, other):
+    if self.release_time == other.release_time:
+      return self.thread_id > other.release_time
+    return self.release_time > other.release_time
+
+
 
 class JobQueue:
     def read_data(self):
         self.num_workers, m = map(int, input().split())
         self.jobs = list(map(int, input().split()))
-        assert m == len(self.jobs)
+        self.size = len(self.jobs)
+        assert m == self.size
 
     def write_response(self):
-        for i in range(len(self.jobs)):
-          print(self.assigned_workers[i], self.start_times[i]) 
+        for worker_id, start_time in self.result:
+          print (worker_id, start_time)
 
     def assign_jobs(self):
         # TODO: replace this code with a faster algorithm.
-        self.assigned_workers = [None] * len(self.jobs)
-        self.start_times = [None] * len(self.jobs)
-        next_free_time = [0] * self.num_workers
-        for i in range(len(self.jobs)):
-          next_worker = 0
-          for j in range(self.num_workers):
-            if next_free_time[j] < next_free_time[next_worker]:
-              next_worker = j
-          self.assigned_workers[i] = next_worker
-          self.start_times[i] = next_free_time[next_worker]
-          next_free_time[next_worker] += self.jobs[i]
+        self.result = []
+        worker_queue = [Worker(i) for i in range(self.num_workers)]
+
+        for job in self.jobs:
+          worker = heapq.heappop(worker_queue)
+
+          self.result.append((worker.thread_id, worker.release_time))
+          worker.release_time += job
+          heapq.heappush(worker_queue, worker)
+        
 
     def solve(self):
         self.read_data()
