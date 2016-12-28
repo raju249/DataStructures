489bac982f3a843f62a164a45d2247ee34845019
Rajendra Kadam <rajendrakadam249@gmail.com>
2016-12-28 20:47:09 +0530

    Chain hash

--- Week 3/hash_chains.py
+++ Week 3/hash_chains.py
@@ -17,7 +17,7 @@ class QueryProcessor:
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
         # store all strings in one list
-        self.elems = []
+        self.buckets = [[] for _ in range(bucket_count)]
 
     def _hash_func(self, s):
         ans = 0
@@ -34,31 +34,46 @@ class QueryProcessor:
     def read_query(self):
         return Query(input().split())
 
-    def process_query(self, query):
-        if query.type == "check":
-            # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
-        else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
-            if query.type == 'find':
-                self.write_search_result(ind != -1)
-            elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
-            else:
-                if ind != -1:
-                    self.elems.pop(ind)
+    def add(self, string):
+        hashed = self._hash_func(string)
+        bucket = self.buckets[hashed]
+        if string not in bucket:
+            self.buckets[hashed] = [string] + bucket
 
-    def process_queries(self):
-        n = int(input())
-        for i in range(n):
-            self.process_query(self.read_query())
+    def delete(self, string):
+        hashed = self._hash_func(string)
+        bucket = self.buckets[hashed]
+        for i in range(len(bucket)):
+            if bucket[i] == string:
+                bucket.pop(i)
+                break
 
-if __name__ == '__main__':
+    def find(self, string):
+        hashed = self._hash_func(string)
+        if string in self.buckets[hashed]:
+            return "yes"
+        return "no"
+
+    def check(self, pos):
+        return self.buckets[pos]
+
+def process_queries(queries):
+    for query in queries:
+        command, arg = query.split()
+        if command == "add":
+            qp.add(arg)
+        elif command == "del":
+            qp.delete(arg)
+        elif command == "find":
+            print(qp.find(arg))
+        elif command == "check":
+            arg = int(arg)
+            print(" ".join(qp.check(arg)))
+
+
+if __name__ == "__main__":
     bucket_count = int(input())
-    proc = QueryProcessor(bucket_count)
-    proc.process_queries()
+    n = int(input())
+    qp = QueryProcessor(bucket_count)
+    queries = [input() for i in range(n)]
+    process_queries(queries)
