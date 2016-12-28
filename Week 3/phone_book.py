48b5f21ef2fee021fb53a3234ed4e8cfd6ffd37f
Rajendra Kadam <rajendrakadam249@gmail.com>
2016-12-28 20:05:09 +0530

    Phone book solution

--- Week 3/phone_book.py
+++ Week 3/phone_book.py
@@ -1,20 +1,22 @@
 # python3
-
+ 
+phone_book = {}
+ 
 class Query:
     def __init__(self, query):
         self.type = query[0]
         self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
-
+ 
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
-
+ 
 def write_responses(result):
     print('\n'.join(result))
-
-def process_queries(queries):
+ 
+'''def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
     contacts = []
@@ -40,8 +42,24 @@ def process_queries(queries):
                     response = contact.name
                     break
             result.append(response)
+    return result'''
+ 
+def process_queries(queries):
+    result = []
+    # Keep list of all existing (i.e. not deleted yet) contacts.
+    contacts = {}
+    for cur_query in queries:
+        if cur_query.type == 'add':
+            contacts[cur_query.number] = cur_query.name
+        elif cur_query.type == 'del':
+            if cur_query.number in contacts:
+                del contacts[cur_query.number]
+        else:
+            response = 'not found'
+            if cur_query.number in contacts:
+                response = contacts[cur_query.number]
+            result.append(response)
     return result
-
+ 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
