f8ddf4a731f0ff1858d12a7a22df20200afe545e
Rajendra Kadam <rajendrakadam249@gmail.com>
2016-12-31 17:13:51 +0530

    Tree orders traversal

--- Week 4/tree-orders.py
+++ Week 4/tree-orders.py
@@ -20,21 +20,58 @@ class TreeOrders:
     self.result = []
     # Finish the implementation
     # You may need to add a new recursive method to do that
-                
+    current = 0
+    stack = []
+    while True:
+      if current != -1:
+        stack.append(current)
+        current = self.left[current]
+      elif stack:
+        current = stack.pop()
+        self.result.append(self.key[current])
+        current = self.right[current]
+      else:
+        break
     return self.result
 
   def preOrder(self):
     self.result = []
     # Finish the implementation
     # You may need to add a new recursive method to do that
-                
+    current = 0
+    stack = []
+    while True:
+      if current != -1:
+        self.result.append(self.key[current])
+        stack.append(current)
+        current = self.left[current]
+      elif stack:
+        current = stack.pop()
+        current = self.right[current]
+      else:
+        break
     return self.result
 
   def postOrder(self):
     self.result = []
     # Finish the implementation
     # You may need to add a new recursive method to do that
-                
+    stack1 = [0]
+    stack2 = []
+    while stack1:
+      current = stack1.pop()
+      stack2.append(self.key[current])
+
+      left = self.left[current]
+      right = self.right[current]
+
+      if left != -1:
+        stack1.append(left)
+      if right != -1:
+        stack1.append(right)
+
+      self.result = reversed(stack2)
+
     return self.result
 
 def main():
